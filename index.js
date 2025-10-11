0e83c73747878656b246f9be2eab049df1a52f48
lorelvis-santos <santoslorelvis@gmail.com>
2025-10-11 08:41:46 -0400

    Add Veterinarian routes

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import dotenv from "dotenv"
 import express from "express";
 import connectToDatabase from "./config/db.js";
 
+// Rutas
+import veterinarianRoutes from "./routes/veterinarian.routes.js"
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 connectToDatabase();
+
+app.use("/api/veterinarians", veterinarianRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
